d3af04d5a2b0a9868ceded90b6e7ac7b4da12bb4
Alex Ellis <alexellis2@gmail.com>
2018-09-11 21:51:07 +0100

    Filter K8s ports/services

--- fn1/handler.go
+++ fn1/handler.go
@@ -3,13 +3,16 @@ package function
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Handle a serverless request
 func Handle(req []byte) string {
 	ret := ""
 	for _, v := range os.Environ() {
-		ret = ret + v + "\n"
+		if strings.Contains(v, "PORT") == false {
+			ret = ret + v + "\n"
+		}
 	}
 
 	return fmt.Sprintf("%s", ret)
