001b394b54472f2f63c04559386909566175a3a8
Alex Ellis <alexellis2@gmail.com>
2018-09-12 20:41:12 +0100

    Use http path
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- octo/handler.rb
+++ octo/handler.rb
@@ -3,10 +3,11 @@ require 'octokit'
 
 class Handler
     def run(req)
+        
 	client = Octokit::Client.new
 
 	# Fetch a user
-	user = client.user req
+	user = client.user ENV["Http_Path"]
 	return user.name
     end
 end
