52f718324268c0ceb09b2bb3031f05be0e906495
Alex Ellis <alexellis2@gmail.com>
2018-09-12 20:45:33 +0100

    Add empty handler
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- octo/handler.rb
+++ octo/handler.rb
@@ -3,11 +3,14 @@ require 'octokit'
 
 class Handler
     def run(req)
-        
-	client = Octokit::Client.new
+		if ENV["Http_Path"].length == 0 then
+			return "To look-up the name of a GitHub user, pass their username as the HTTP path such as /alexellis"
+		end
 
-	# Fetch a user
-	user = client.user ENV["Http_Path"]
-	return user.name
+		client = Octokit::Client.new
+
+		# Fetch a user
+		user = client.user ENV["Http_Path"]
+		return user.name
     end
 end
