665d9597547d8e0425630ba2dbb73c2951a61ce2
Alex Ellis <alexellis2@gmail.com>
2018-09-12 20:55:22 +0100

    Pass trimmed username to client
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- octo/handler.rb
+++ octo/handler.rb
@@ -3,16 +3,16 @@ require 'octokit'
 
 class Handler
 	def run(req)
-		user = ENV["Http_Path"].gsub(/#{Regexp.escape('/')}/, "")
+		user_name = ENV["Http_Path"].gsub(/#{Regexp.escape('/')}/, "")
 
-		if user.length == 0 then
+		if user_name.length == 0 then
 			return "To look-up the name of a GitHub user, pass their username as the HTTP path such as /alexellis"
 		end
 
 		client = Octokit::Client.new
 
 		# Fetch a user
-		user = client.user ENV["Http_Path"]
+		user = client.user user_name
 		return user.name
     end
 end
