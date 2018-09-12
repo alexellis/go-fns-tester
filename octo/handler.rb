6e981d82aa9cc19d98cc4a7e2b4c3f8c5fabc2a0
Alex Ellis <alexellis2@gmail.com>
2018-09-12 20:50:45 +0100

    Trim trailing slash
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- octo/handler.rb
+++ octo/handler.rb
@@ -2,8 +2,10 @@ require 'octokit'
 
 
 class Handler
-    def run(req)
-		if ENV["Http_Path"].length == 0 or ENV["Http_Path"] == "/" then
+	def run(req)
+		user = ENV["Http_Path"].gsub(/#{Regexp.escape('\/')}/, "")
+
+		if user.length == 0 then
 			return "To look-up the name of a GitHub user, pass their username as the HTTP path such as /alexellis"
 		end
 
